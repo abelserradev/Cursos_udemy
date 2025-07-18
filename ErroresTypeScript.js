99a38b0d5cbd2e5ec43b11ac4e3be06bb393c55a
Abel Serra <aj_serra@hotmail.com>
2025-07-18 10:05:32 +0200

    Errores en typescript

---
+++ ErroresTypeScript.js
@@ -0,0 +1,7 @@
+// explicito
+var nombre = "Pedro";
+//implicito
+var apellido = "Picapiedra";
+var casado = true;
+var edad = 35;
+edad = "Muy viejito";
---
+++ ErroresTypescript.ts
@@ -0,0 +1,8 @@
+// explicito
+let nombre: string = "Pedro";
+
+//implicito
+let apellido = "Picapiedra"
+let casado = true;
+let edad = 35;
+edad = "Muy viejito";
